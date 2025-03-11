8bed6bddf43959721bba9b7d438359285f09dc6b
maksimhrybok <maxgribok1204@gmail.com>
2025-03-11 23:08:47 +0200

    .

--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +13,64 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        private const string APP_NAME = "UNLTIMATE_PREDICTOR";
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void splitContainer1_SplitterMoved(object sender, SplitterEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = APP_NAME;
+        }
+
+        private async void bPredict_Click(object sender, EventArgs e)
+        {
+            bPredict.Enabled = false;
+           await Task.Run(() =>
+            {
+                for (int i = 1; i < 100; i++)
+                {
+                    this.Invoke(new Action(() =>
+                    {
+                        UpdateProgressBar(i);
+                        this.Text = $"{i}%";
+                    }));
+                    Thread.Sleep(20);
+
+                }
+
+            });
+
+            MessageBox.Show("prediction");
+
+            progressBar1.Value = 0;
+            this.Text = APP_NAME;
+            bPredict.Enabled = true;
+
+
+        }
+        private void UpdateProgressBar(int i)
+        {
+            if (i == progressBar1.Maximum)
+            {
+                progressBar1.Maximum = i + 1;
+                progressBar1.Value = i + 1;
+                progressBar1.Maximum = i;
+
+            }
+            else
+            {
+                progressBar1.Value = i + 1;
+            }
+
+            progressBar1.Value = i;
+
         }
     }
 }
