869054021dc51f213ae494db9392f38cf9f140b4
maksimhrybok <maxgribok1204@gmail.com>
2025-03-12 01:46:26 +0200

    .

--- Form1.cs
+++ Form1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -14,6 +16,10 @@ namespace WindowsFormsApp1
     public partial class Form1 : Form
     {
         private const string APP_NAME = "UNLTIMATE_PREDICTOR";
+        private readonly string PREDICTIONS_CONFIG_PATH = $"{Environment.CurrentDirectory}\\predictionsConfig.json";
+        private string[] _predictions;
+        private Random _random = new Random();
+
         public Form1()
         {
             InitializeComponent();
@@ -27,6 +33,29 @@ namespace WindowsFormsApp1
         private void Form1_Load(object sender, EventArgs e)
         {
             this.Text = APP_NAME;
+
+            try
+            {
+                 var data = File.ReadAllText(PREDICTIONS_CONFIG_PATH);
+
+                _predictions = JsonConvert.DeserializeObject<string[]>(data);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if(_predictions == null)
+                {
+                    Close();
+                }
+                else if (_predictions.Length == 0)
+                {
+                    MessageBox.Show("Predictions have done, sorry.");
+                    Close();
+                }
+            }
         }
 
         private async void bPredict_Click(object sender, EventArgs e)
@@ -47,7 +76,11 @@ namespace WindowsFormsApp1
 
             });
 
-            MessageBox.Show("prediction");
+            var index = _random.Next(_predictions.Length);
+
+            var prediction = _predictions[index];
+
+            MessageBox.Show(prediction);
 
             progressBar1.Value = 0;
             this.Text = APP_NAME;
